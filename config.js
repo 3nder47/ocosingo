7d71d108fbc9d80a7518d985d91f9f09ad0eb0a0
3nder47 <3nder47@gmail.com>
2026-08-30 01:58:03 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de Tienda Ocosingo. Este archivo NO se reemplaza al actualizar la app.
 window.OC_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyw1qSLB5efvhWqHHQExE9BwPovqKv51R-v1sKbxMrbCaZNmUooRdPluyOD2JskoVj8/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwzP9LS3Et4hPI1VB-34O4l9fF-cBcnbO0GnYPOvT3g-JWw0A9jSeskEg6MZ8XfJyu_/exec',
   API_TOKEN: 'iQ3FfD2Qfdo10Qubs3CyIp03LjF1COgx'
 };
